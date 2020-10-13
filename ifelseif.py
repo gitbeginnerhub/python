b47b1679fc3bafcb02c42639647afe0edb443ccb
gitbeginnerhub <dhwanishh2@gmail.com>
2020-10-13 13:04:19 -0400

    trial

---
+++ ifelseif.py
@@ -0,0 +1,10 @@
+# Python program to illustrate 
+# selection statement 
+
+num1 = int(input("enter your num: "))
+if(num1>12): 
+	print("Num1 is good") 
+elif(num1>35): 
+	print("Num2 is not gooooo....") 
+else: 
+	print("Num2 is great") 
