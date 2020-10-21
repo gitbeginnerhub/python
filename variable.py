5d1a6ef577272b63b810e90ee47c003d4eda80da
gitbeginnerhub <64223101+gitbeginnerhub@users.noreply.github.com>
2020-10-21 10:12:28 -0400

    if/else

---
+++ variable.py
@@ -0,0 +1,11 @@
+#direct initialisation variable assignment
+
+a = 5.5
+
+print ('the value of  is= ' + str(a))
+
+#if-else one liner
+
+b = 1 if 20 > 10 else 0
+
+print ('the value of b is : ' +str(b))
